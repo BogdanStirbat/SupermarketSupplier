aa6badde41cafebbb61ff92d81444b3a5a2c1d5f
Bogdan Stirbat <bogdan.stirbat@gmail.com>
2020-06-07 13:14:19 +0300

    init backend application

---
+++ backend/src/main/java/com/bstirbat/supermarketsupplier/SupermarketsupplierApplication.java
@@ -0,0 +1,13 @@
+package com.bstirbat.supermarketsupplier;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SupermarketsupplierApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SupermarketsupplierApplication.class, args);
+	}
+
+}
---
+++ backend/src/test/java/com/bstirbat/supermarketsupplier/SupermarketsupplierApplicationTests.java
@@ -0,0 +1,13 @@
+package com.bstirbat.supermarketsupplier;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SupermarketsupplierApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
